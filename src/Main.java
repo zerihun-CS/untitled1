d9d9119d18a8e7d2362d42808aa6246d59eeaef4
zerihun-cs <zerihun.cs@gmail.com>
2023-06-17 16:34:02 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,40 @@
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int age  = 24;
+//        variable x = (expression) ? value if true : value if false
+        String isAdult = (age > 18) ? "Adult" : "UnderAge";
+        boolean isEthiopian = true;
+        boolean hasAnOffice = false;
+
+        boolean hasCriminalRecord = true;
+
+//       boolean canHaveLicence  = (!hasCriminalRecord &&
+//               (isEthiopian || isAdult || hasAnOffice));
+
+
+       int a = 7;
+       int b= 10;
+
+        b+=a;
+
+        System.out.println(b);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ src/StringExample.java
@@ -0,0 +1,35 @@
+import java.time.Year;
+import java.util.Random;
+import java.util.Scanner;
+
+public class StringExample {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+//
+//        System.out.println("please insert your telegram " +
+//                "username ?");
+//        String username =  scan.nextLine();
+//        System.out.println((username.contains("@"))?
+//                "invalid username":"user accepted");
+//
+//
+//        System.out.println("please insert date of birth");
+//        int dob = scan.nextInt();
+//        int age  = 2023- dob;
+//        //(variable x = (expression) ?
+//        // value if true : value if false)
+//        String accountStatus =(age < 18 )?
+//            "underage cant create an account":
+//                "your account has been created";
+//
+//        System.out.println(accountStatus);
+        System.out.println("please insert a number ?");
+        int userInput = scan.nextInt();
+        int randomNumber = (int)Math.round(Math.random()*10);
+        System.out.println((userInput ==randomNumber
+                ?"congra":"try again"));
+    }
+
+
+}
